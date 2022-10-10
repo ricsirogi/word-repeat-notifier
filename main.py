7bb24bc71fc283f4c03605853c044580fb8c0f66
ricsirogi <rikike888@gmail.com>
2022-10-10 21:17:18 +0200

    Updated main.py again
    - text variable is way more easy to work with now
    - word counting is way more accurate, might need some more work, but looks ok for now
    - fixed a bug that came up with the new text variable
    - added text, when there are no repeated words

--- main.py
+++ main.py
@@ -15,30 +15,42 @@ def StartSearch():
     """
 
     # Declare variables 
-    text = textBox.get("0.0", "end") # the content of the text box
+
+    # | the content of the text box 
+    # | (there's always a "\n" at the end of the text)
+    # | (remove extra spaces at the end of text, to prevent them from )
+    # V (.lower() for easier handling of the text) 
+    text = textBox.get("0.0", "end").rstrip("\n").rstrip(" ").lower()
     words = [] # list of all the words
     temp = "" # temporary variable that helps with 
     isSpace = False # temporary variable, that helps with preventing registering double spaces as one word
-    wordCounter = 0 # counts the total wordsd
+    wordCounter = 1 # counts the total word  (there's plus one, because the wor counter doesn't count the last word)
     sentenceCounter = 0 # counts the total sentences
-
+    endOfSentenceCharacters = [".", "?", "!"]
+    repeatedWordsLabelList = [] # doesn't do anything for now, but I will rework the way I display the repeated words, so I can display more words.
     # Loops thorough every character of the text
     for counter, i in enumerate(text):
         if i != " ":
-            temp += i
+            if i != "\n" and i not in endOfSentenceCharacters:
+                temp += i
             isSpace = False
         else:
             if not isSpace:
-                words.append(temp.lower())
+                words.append(temp)
                 temp = ""
-                isSpace = True
                 wordCounter += 1
+                isSpace = True
+                
         if counter == len(text)-1:
             words.append(temp)
             temp = ""
-        if i == "." or i == "?" or i == "!":
-            if text[counter+1] == " " or text[counter+1] != "." and not text[counter+1].isnumeric():
+        if i in endOfSentenceCharacters:
+            try:
+                if text[counter+1] == " " or text[counter+1] != "." and not text[counter+1].isnumeric():
+                    sentenceCounter += 1
+            except IndexError:
                 sentenceCounter += 1
+    
     words[len(words)-1] = words[len(words)-1].rstrip()
     totalWordsLabel["text"] = f"Total words: {wordCounter}\nTotal sentences: {sentenceCounter}"
 
@@ -59,13 +71,18 @@ def StartSearch():
 
     oRepeatedWords = dict(sorted(repeatedWords.items(), key=lambda x:x[1], reverse=True))
 
+    repeatedWordsLabel["text"] = ""
     for i in oRepeatedWords:
         repeatedWordsLabel["text"] += f"{i}: {oRepeatedWords[i]}\n"
+
+    if repeatedWordsLabel["text"] == "":
+        repeatedWordsLabel["text"] = "Wow! No repeated words!"
 
 textBox = tk.Text(win, height=3, width=50)
 startButton = tk.Button(win, text="Start!", command=lambda:StartSearch())
 totalWordsLabel = tk.Label(win)
 repeatedWordsLabel = tk.Label(win)
+
 
 textBox.grid(row=0, column=0)
 startButton.grid(row=0, column=1)
@@ -73,3 +90,4 @@ totalWordsLabel.grid(row=1, column=0, sticky="w")
 repeatedWordsLabel.grid(row=2, column=0, sticky="w")
 
 win.mainloop()
+
